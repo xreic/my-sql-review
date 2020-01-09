1017cdcadc8fbeedeaf35f424271d15bb7630a54
Late1nAutumn <forestpu@163.com>
2020-01-09 00:14:04 -0800

    step2

--- database/schema.sql
+++ database/schema.sql
@@ -3,3 +3,10 @@ CREATE DATABASE ball;
 /*
 mysql -u forest < database/schema.sql
 */
+USE ball
+
+CREATE TABLE tennis (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  item_name VARCHAR(255) NOT NULL,
+  amount INT NOT NULL
+);
