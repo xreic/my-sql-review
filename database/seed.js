f3323d433ee9660d7d81cec5d19ae32f5f2e99d0
Late1nAutumn <forestpu@163.com>
2020-01-09 00:29:17 -0800

    step3

--- database/seed.js
+++ database/seed.js
@@ -1,1 +1,14 @@
-// TODO
+const db = require("./index.js");
+const temp = [
+  { name: "porsche", amount: 0 },
+  { name: "zerglings", amount: 2 },
+  { name: "RTX2070", amount: 1 },
+  { name: "forest", amount: 0.5 },
+  { name: "hrla", amount: 34 }
+];
+
+temp.forEach(({ name, amount }) => {
+  db.query(
+    `INSERT INTO tennis (item_name, amount) VALUES ("${name}", ${amount});`
+  );
+});
